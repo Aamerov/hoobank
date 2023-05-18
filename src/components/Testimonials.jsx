4a4e1b013d8e6aa363c1f2e1a5d8abf9014ff716
Aamer Al Mahmoud <aamersyrai2000@gmail.com>
2023-05-18 10:21:50 +0300

    Fix-4

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,6 +5,7 @@ import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
   <section
+    id="clients"
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
   >
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
