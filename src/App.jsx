28d9a7182cc5deead7ed2556dac358dcc73beab1
Aamer Al Mahmoud <aamersyrai2000@gmail.com>
2023-05-18 10:14:45 +0300

    Fix-3

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Navbar />
+        <NavBar />
       </div>
     </div>
 
